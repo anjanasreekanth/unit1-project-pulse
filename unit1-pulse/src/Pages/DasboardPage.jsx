e5ded4912e1fd0421a38c62e2860edd493680d2c
Anjana Sreekanth <anjanavshenoy@gmail.com>
2026-07-07 12:45:53 -0500

    Added logic to Table and activity Form

--- unit1-pulse/src/Pages/DasboardPage.jsx
+++ unit1-pulse/src/Pages/DasboardPage.jsx
@@ -1,22 +1,81 @@
+import { useState } from "react";
 import ActivityLogForm from "../components/ActivityLogForm";
 import ActivityTable from "../components/ActivityTable";
 import AverageScoreCard from "../components/AverageScoreCard";
 import StreakCard from "../components/StreakCard";
 function DashboardPage() {
+  //1. Create state for integration
+  const [activities, setActivities] = useState([
+    {
+      id: 1,
+      date: "2026-06-20",
+      activity: "Running",
+      duration: "1 hour",
+      score: 8,
+      water: 2500,
+      sleep: 7,
+    },
+    {
+      id: 2,
+      date: "2026-06-21",
+      activity: "Walking",
+      duration: "30 mts",
+      score: 7,
+      water: 1800,
+      sleep: 8,
+    },
+    {
+      id: 3,
+      date: "2026-06-22",
+      activity: "Meditation",
+      duration: "30 mts",
+      score: 7,
+      water: 2000,
+      sleep: 7,
+    },
+  ]);
+  //console.log(activities);
+  // 2. event handler to manage add acitivity
+  const handleAddActivity = (newActivity) => {
+    // add new activity with id
+    const newActivityData = {
+      ...newActivity,
+      id: Date.now(), // generating dynamic id
+      score: Math.floor(Math.random() * 10) + 5, // mock score generateor
+    };
+
+    // update the sate
+    setActivities([...activities, newActivityData]);
+    alert("Activity Log Added!");
+  };
+
+  //3. delete activity handler
+  const handleDeleteActivity = (idToDelete) => {
+    // filter activity that is not maching the id
+    const updatedActivities = activities.filter(
+      (activity) => activity.id !== idToDelete,
+    );
+    setActivities(updatedActivities);
+    alert("Activity Deleted!");
+  };
+
   return (
     <div className="dashboard-grid">
-        {/** left column */}
+      {/** left column */}
       <div className="dashboard-column left-column">
-        <ActivityLogForm />
+        <ActivityLogForm onAddActivity={handleAddActivity} />
       </div>
-        {/** right column */}
+      {/** right column */}
       <div className="dashboard-column right-column">
         <AverageScoreCard />
         <div className="streak-card">
-            <StreakCard />
+          <StreakCard />
         </div>
         <div className="activity-table-container">
-            <ActivityTable />
+          <ActivityTable
+            activities={activities}
+            onDeleteActivity={handleDeleteActivity}
+          />
         </div>
       </div>
     </div>
